984e4d062e65baa0d6b270d58fea7d6c3011c9ac
Haley <alicelai2920@gmail.com>
2019-12-15 22:54:05 -0800

    Initial Commit

---
+++ Skip_List/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Skip_List
+//
+//  Created by Haley Lai on 12/15/19.
+//  Copyright © 2019 Haley Lai. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+   // insert code here...
+   std::cout << "Hello, World!\n";
+   return 0;
+}
